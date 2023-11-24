d5ea6273a1cd0cbc49e298e179a586c78759aa91
Meluhn <tarankamireddy@gmail.com>
2023-11-23 21:39:21 -0500

    footer link works

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,26 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { styles } from '../styles';
-import { menu, close } from '../assets';
-import { navLinks } from '../constants';
+import { planning } from '../constants';
 
 const Footer = () => {
 
   return (
     <div className="flex flex-wrap justify-center bg-tertiary p-2">
         <a className='m-5 hover:font-bold hover:underline'target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/3crGWCaz25kZ55FCA">Rock Ridge High School</a>
-        <p className='m-5 hover:font-bold hover:underline'>Information Packet</p>
+        <Link
+              to={`${planning}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="m-5 hover:font-bold hover:underline"
+            >
+              <span>Info Packet</span>
+        </Link>        
         <a className='m-5 hover:font-bold hover:underline' target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/phoenix.hacks/'>Instagram</a>
         <Link className='m-5 hover:font-bold hover:underline' to='javascript:void(0)'onClick={() => window.location = 'mailto:yourmail@domain.com'}>
           phoenixhacks2023@gmail.com
         </Link>
+
     </div>
   )
 }
