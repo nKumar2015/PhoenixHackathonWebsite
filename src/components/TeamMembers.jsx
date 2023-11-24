cca63e471fb2c0200b9d1796e5c9a76ffd912efc
Meluhn <tarankamireddy@gmail.com>
2023-11-23 21:25:28 -0500

    testing mobile

--- src/components/TeamMembers.jsx
+++ src/components/TeamMembers.jsx
@@ -16,7 +16,7 @@ const PersonCard = ({ name, image, role }) => {
   return (
     <Tilt
       option={{ max: 45, scale: 1, speed: 450 }}
-      className="bg-tertiary p-3 xs:p-5 sm:min-w-[285px] xs:min-w-[150px] min-w-[100px] rounded-2xl"
+      className="bg-tertiary p-3 xs:p-5 sm:w-[285px] xs:w-[150px] w-[100px] rounded-2xl"
     >
       <div className="flex flex-col relative items-center">
         <img
